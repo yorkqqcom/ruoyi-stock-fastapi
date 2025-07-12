0bb63c9b5b897ecd9112debb0ff7ab207b37fc15
Y2712 <27123167@qq.com>
2025-07-12 23:48:16 +0800

    ruoyi-stock-fastAPI v0.0.5，增加概念板块层级分析功能

--- ruoyi-fastapi-backend/sql/ruoyi-fastapi.sql
+++ ruoyi-fastapi-backend/sql/ruoyi-fastapi.sql
@@ -264,6 +264,7 @@ insert into sys_menu values('1062', '个股分析', '1061', '0', 'analyzer', 'st
 insert into sys_menu values('1063', '方差比率和自相关发现趋势与反转', '1061', '1', 'predictability', 'stock/predictability/index', NULL, '', '1', '0', 'C', '0', '0', NULL, 'chart', 'admin', '2025-05-24 12:06:21', 'admin', '2025-05-24 17:33:19', '');
 insert into sys_menu values('1064', '每日盘点', '0', '1', 'marketreview', 'stock/marketreview/index', NULL, '', '1', '0', 'C', '0', '0', NULL, 'button', 'admin', '2025-05-27 12:52:05', 'admin', '2025-05-27 12:53:23', '');
 insert into sys_menu values('1065', 'LSTM均值回归', '1061', '4', 'ai_market', 'stock/ai-market/index', NULL, '', '1', '0', 'C', '0', '0', NULL, 'chart', 'admin', '2025-05-29 20:35:35', 'admin', '2025-05-29 21:04:35', '');
+insert into sys_menu values('1066', '概念板块层级分析', '1061', '5', 'relations', 'stock/conceptrelations/index', NULL, '', '1', '0', 'C', '0', '0', NULL, 'cascader', 'admin', '2025-07-12 22:08:15', 'admin', '2025-07-12 22:09:30', '');
 
 -- ----------------------------
 -- 6、用户和角色关联表  用户N-1角色
